e590d496bbf4c0294eaa1dcb4a2fa160d298e445
relativvv <robin.schulte01@googlemail.com>
2023-05-29 21:56:01 +0200

    add libraries

--- src/app/components/shared/navigation/navigation.component.ts
+++ src/app/components/shared/navigation/navigation.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavigationItem } from 'src/app/models/nav-item.model';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-navigation',
@@ -10,12 +11,29 @@ export class NavigationComponent {
 
   public navItems: NavigationItem[];
 
-  constructor() {
+  constructor(
+    private readonly router: Router
+  ) {
     this.navItems = [
       { displayName: 'Home', iconName: 'home' },
       { displayName: 'Projects', iconName: 'code' },
       { displayName: 'Skills', iconName: 'fitness_center' },
       { displayName: 'Tech', iconName: 'computer' },
-    ]
+    ];
+
+    switch(this.router.url) {
+      case '/projects':
+        break;
+
+      case '/skills':
+        break;
+
+      case '/tech':
+        break;
+
+      case '/home':
+      default:
+        break;
+    }
   }
 }
--- src/app/components/sites/sites.module.ts
+++ src/app/components/sites/sites.module.ts
@@ -3,15 +3,17 @@ import {CommonModule, NgOptimizedImage} from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from '../shared/shared.module';
 import { ProfileComponent } from './home/profile/profile.component';
+import {NgxTypedJsModule} from "ngx-typed-js";
 
 @NgModule({
   declarations: [
     HomeComponent,
     ProfileComponent
   ],
-    imports: [
-        CommonModule,
-        NgOptimizedImage,
-    ]
+  imports: [
+    CommonModule,
+    NgOptimizedImage,
+    NgxTypedJsModule,
+  ]
 })
 export class SitesModule { }
