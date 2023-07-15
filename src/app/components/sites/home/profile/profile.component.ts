809278457f92bc4ed9a85e6e91fec8796942a8f8
relativvv <robin.schulte01@googlemail.com>
2023-07-16 01:35:07 +0200

    add linkedin

--- src/app/components/sites/home/profile/profile.component.ts
+++ src/app/components/sites/home/profile/profile.component.ts
@@ -22,11 +22,4 @@ export class ProfileComponent implements OnInit {
 
     this.cookieService.set("typedJsSucceed", "true", 1);
   }
-
-  text: string = "Hello! I'm Robin, an experienced full stack developer, with expertise in Angular and Symfony frameworks. I have a strong background in building robust and user-friendly web applications.\n" +
-    "Currently, I work at a leading software company, contributing to the development of e-commerce solutions.<br /> My skills in <b>Angular and Symfony</b> have been instrumental in helping businesses thrive in the digital landscape.\n" +
-    "I have a solid understanding of front-end and back-end development, including <b>HTML, LESS, and TypeScript</b>. I also have experience with databases like <b>MySQL</b> for efficient data handling.\n" +
-    "What sets me apart is my problem-solving mindset. I thrive in challenging situations, always seeking innovative solutions. Staying up-to-date with industry trends and technologies is crucial to me.\n" +
-    "Beyond programming, I'm an avid learner, constantly exploring diverse subjects to enhance my creativity. Thank you for visiting my portfolio, and I look forward to collaborating on exciting projects." +
-    "Feel free to <a href='mailto: r.schulte@relativv.de'>reach out to me</a> for inquiries or opportunities. Let's create something amazing together!"
 }
